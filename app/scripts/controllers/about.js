da2901b4161606a25b6e5035db74b6b44a84b783
James Strassburg <jstrassburg@gmail.com>
2016-03-10 16:18:39 -0600

    Yeoman generated site

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quoteToneApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the quoteToneApp
+ */
+angular.module('quoteToneApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quoteToneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quoteToneApp
+ */
+angular.module('quoteToneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
