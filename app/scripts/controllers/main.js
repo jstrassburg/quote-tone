4dd7dacac9d0629bb46b0df9348f8e17053c960d
James Strassburg <jstrassburg@gmail.com>
2016-03-10 23:02:06 -0600

    Making the quotes an ng-repeast on an array

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,15 +10,23 @@
 angular.module('quoteToneApp')
   .controller('MainCtrl', function ($scope) {
     $scope.emotionalLables =['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness'];
-    $scope.emotionalData = [
-        [0.65, 0.59, 0.90, 0.81, 0.56]
-    ];
     $scope.writingLables =['Analytical', 'Confident', 'Tentative'];
-    $scope.writingData = [
-        [0.25, 0.09, 0.99]
-    ];
     $scope.socialLables =['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Neuroticism'];
-    $scope.socialData = [
-        [0.69, 0.22, 0.82, 0.42, 0.02]
+
+    $scope.quotes = [
+      {
+        text: 'Quote 1',
+        author: 'Author 1',
+        emotionalData: [[0.65, 0.59, 0.90, 0.81, 0.56]],
+        writingData: [[0.25, 0.22, 0.84]],
+        socialData: [[0.69, 0.22, 0.82, 0.42, 0.02]]
+      },
+      {
+        text: 'Quote 2',
+        author: 'Author 2',
+        emotionalData: [[0.99, 0.22, 0.33, 0.45, 0.6]],
+        writingData: [[0.95, 0.52, 0.8]],
+        socialData: [[0.29, 0.62, 0.12, 0.12, 0.12]]
+      }
     ];
   });
