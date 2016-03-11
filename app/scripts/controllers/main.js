2945e7c58902ad3c1ad6be800c17b7eb6f712380
James Strassburg <jstrassburg@gmail.com>
2016-03-10 21:33:41 -0600

    Basic design and style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the quoteToneApp
  */
 angular.module('quoteToneApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.emotionalLables =['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness'];
+    $scope.emotionalData = [
+        [0.65, 0.59, 0.90, 0.81, 0.56]
+    ];
+    $scope.writingLables =['Analytical', 'Confident', 'Tentative'];
+    $scope.writingData = [
+        [0.25, 0.09, 0.99]
+    ];
+    $scope.socialLables =['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Neuroticism'];
+    $scope.socialData = [
+        [0.69, 0.22, 0.82, 0.42, 0.02]
     ];
   });
